339542dd824c59eba5161b306d8f767676339d03
eric santos <eric355402@yahoo.com>
2021-09-20 13:45:41 -0600

    addapted the like button with framer-motion for animation

--- src/components/NasaCard.jsx
+++ src/components/NasaCard.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
+import { motion } from "framer-motion"
 
 const NasaCard = ({hdurl,title, date, explanation}) => {
 
@@ -13,52 +14,25 @@ const NasaCard = ({hdurl,title, date, explanation}) => {
       <Card.Group centered>
         <Card fluid>
           <Image src={hdurl} wrapped ui={false} alt='nasa' />
-    <Card.Content>
-      <Card.Header>{title}</Card.Header>
-      <Card.Meta>
-        <span className='date'>{date}</span>
-      </Card.Meta>
-      <Card.Description>
-       {explanation}
-      </Card.Description>
-    </Card.Content>
-        <Card.Content extra onClick={handleClick}>
-          {/* conditional rendering based on state, removed button due to deprecated FindDOMNode React error */}
-      {isLiked ?
-            <Icon name='like'>Like</Icon>
-       :
-            <Icon name='cancel'>Unlike</Icon>
-      }
-    </Card.Content>
-  </Card>
-        </Card.Group>
+          <Card.Content>
+          <Card.Header>{title}</Card.Header>
+          <Card.Meta>
+            <span className='date'>{date}</span>
+          </Card.Meta>
+          <Card.Description>
+            {explanation}
+          </Card.Description>
+          </Card.Content>
+          <Card.Content extra onClick={handleClick}>
+            {/* conditional rendering based on state, removed button due to deprecated FindDOMNode React error */}
+            {isLiked ?
+              <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>< Icon name='like'/></motion.button> : <motion.button whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}><Icon name='cancel'/></motion.button>
+            }
+          </Card.Content>
+        </Card>
+      </Card.Group>
     )
 }
 
 export default NasaCard
-
-// import React from 'react'
-// import { Card, Icon, Image } from 'semantic-ui-react'
-
-// const CardExampleCard = () => (
-  // <Card>
-  //   <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-  //   <Card.Content>
-  //     <Card.Header>Matthew</Card.Header>
-  //     <Card.Meta>
-  //       <span className='date'>Joined in 2015</span>
-  //     </Card.Meta>
-  //     <Card.Description>
-  //       Matthew is a musician living in Nashville.
-  //     </Card.Description>
-  //   </Card.Content>
-  //   <Card.Content extra>
-  //     <a>
-  //       <Icon name='user' />
-  //       22 Friends
-  //     </a>
-  //   </Card.Content>
-  // </Card>
-// )
-
-// export default CardExampleCard
