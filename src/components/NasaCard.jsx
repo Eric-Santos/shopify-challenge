ace7c3937d34d62251e48ba58bd5bec7d55ec7bf
eric santos <eric355402@yahoo.com>
2021-09-20 13:48:11 -0600

    general clean up, no errors

--- src/components/NasaCard.jsx
+++ src/components/NasaCard.jsx
@@ -1,38 +1,46 @@
 import React, {useState} from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
-import { motion } from "framer-motion"
+import { Card, Image } from "semantic-ui-react";
+import { motion } from "framer-motion";
 
-const NasaCard = ({hdurl,title, date, explanation}) => {
-
-  const [isLiked, setIsLiked] = useState(true)
+const NasaCard = ({ hdurl, title, date, explanation }) => {
+  const [isLiked, setIsLiked] = useState(true);
 
   const handleClick = () => {
-    setIsLiked(!isLiked)
-  }
+    setIsLiked(!isLiked);
+  };
 
-    return (
-      <Card.Group centered>
-        <Card fluid>
-          <Image src={hdurl} wrapped ui={false} alt='nasa' />
-          <Card.Content>
+  return (
+    <Card.Group centered>
+      <Card fluid>
+        <Image src={hdurl} wrapped ui={false} alt="nasa" />
+        <Card.Content>
           <Card.Header>{title}</Card.Header>
           <Card.Meta>
-            <span className='date'>{date}</span>
+            <span className="date">{date}</span>
           </Card.Meta>
-          <Card.Description>
-            {explanation}
-          </Card.Description>
-          </Card.Content>
-          <Card.Content extra onClick={handleClick}>
-            {/* conditional rendering based on state, removed button due to deprecated FindDOMNode React error */}
-            {isLiked ?
-              <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>< Icon name='like'/></motion.button> : <motion.button whileHover={{ scale: 1.1 }}
-    whileTap={{ scale: 0.9 }}><Icon name='cancel'/></motion.button>
-            }
-          </Card.Content>
-        </Card>
-      </Card.Group>
-    )
-}
+          <Card.Description>{explanation}</Card.Description>
+        </Card.Content>
+        <Card.Content extra onClick={handleClick}>
+          {/* conditional rendering based on state, removed button due to deprecated FindDOMNode React error */}
+          {isLiked ? (
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Like
+            </motion.button>
+          ) : (
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Dislike
+            </motion.button>
+          )}
+        </Card.Content>
+      </Card>
+    </Card.Group>
+  );
+};
 
 export default NasaCard
